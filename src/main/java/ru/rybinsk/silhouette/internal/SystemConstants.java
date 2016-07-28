9e1f55fabff7318047ea95300afdcd1c03b61f3f
ASmirnov <aleksey.smirnov89@gmail.com>
2016-07-28 17:47:06 +0300

    up version

--- src/main/java/ru/rybinsk/silhouette/internal/SystemConstants.java
+++ src/main/java/ru/rybinsk/silhouette/internal/SystemConstants.java
@@ -16,6 +16,6 @@ public class SystemConstants {
     public static final String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "1234";
-    public static final String VERSION = "2.2.1";
+    public static final String VERSION = "2.2.2";
 
 }
